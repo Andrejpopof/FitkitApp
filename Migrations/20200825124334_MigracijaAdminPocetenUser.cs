b4eeb3f6cc9eb2da98110de1a669b7796c413a9c
Andrejpopof <andrejpopov.1430@gmail.com>
2020-08-25 14:49:21 +0200

    added Admin in SeedData

---
+++ Migrations/20200825124334_MigracijaAdminPocetenUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FitKitApp.Migrations
+{
+    public partial class MigracijaAdminPocetenUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -11,7 +11,36 @@ namespace FitKitApp.Models
 {
     public class SeedData
     {
-            public static void Initialize(IServiceProvider serviceProvider)
+
+        public static async Task CreateUserRoles(IServiceProvider serviceProvider)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<AppUser>>();
+
+            IdentityResult roleResult;
+            var roleCheck = await RoleManager.RoleExistsAsync("Admin");
+            if (!roleCheck)
+            {
+                roleResult = await RoleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+
+            AppUser user = await UserManager.FindByEmailAsync("admin@gmail.com");
+            if (user == null)
+            {
+                var User = new AppUser();
+                User.Email = "admin@gmail.com";
+                User.UserName = "adminFitkit";
+                User.Role = "Admin";
+                string userPWD = "admin123";
+                IdentityResult chkUser = await UserManager.CreateAsync(User, userPWD);
+                //Add default User to Role Admin      
+                if (chkUser.Succeeded)
+                {
+                    var result1 = await UserManager.AddToRoleAsync(User, "Admin");
+                }
+            }
+        }
+        public static void Initialize(IServiceProvider serviceProvider)
         {
             using (var context = new FitKitAppContext(serviceProvider.GetRequiredService<DbContextOptions<FitKitAppContext>>()))
             {
